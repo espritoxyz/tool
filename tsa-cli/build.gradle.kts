af14148340f1d6fcab568c57d3dedefe8da26dd6
Yury Kamenev <yurkam447@gmail.com>
2024-07-29 17:01:50 +0300

    Added shadowJar task

--- tsa-cli/build.gradle.kts
+++ tsa-cli/build.gradle.kts
@@ -1,5 +1,21 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("tsa.kotlin-conventions")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+val mainClassName = "org.ton.MainKt"
+
+tasks.register<JavaExec>("run") {
+    classpath = sourceSets.main.get().runtimeClasspath
+    mainClass = mainClassName
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = mainClassName
+    }
 }
 
 dependencies {
@@ -9,7 +25,10 @@ dependencies {
 
 }
 
-tasks.register<JavaExec>("run") {
-    classpath = sourceSets.main.get().runtimeClasspath
-    mainClass = "org.ton.MainKt"
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("")
+    val implementation = project.configurations["implementation"].dependencies.toSet()
+    val runtimeOnly = project.configurations["runtimeOnly"].dependencies.toSet()
+    val dependencies = (implementation + runtimeOnly)
+    project.configurations.shadow.get().dependencies.addAll(dependencies)
 }
