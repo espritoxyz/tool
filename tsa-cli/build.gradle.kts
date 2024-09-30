35909f191a5574accdd56cb3bd1a29a00968e854
Yury Kamenev <yurkam447@gmail.com>
2024-09-30 11:41:22 +0300

    Added fat jar task

--- tsa-cli/build.gradle.kts
+++ tsa-cli/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("tsa.kotlin-conventions")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 dependencies {
@@ -11,7 +14,23 @@ dependencies {
 
 }
 
+val mainClassName = "org.ton.MainKt"
+
 tasks.register<JavaExec>("run") {
     classpath = sourceSets.main.get().runtimeClasspath
-    mainClass = "org.ton.MainKt"
+    mainClass = mainClassName
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = mainClassName
+    }
+}
+
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("")
+    val implementation = project.configurations["implementation"].dependencies.toSet()
+    val runtimeOnly = project.configurations["runtimeOnly"].dependencies.toSet()
+    val dependencies = (implementation + runtimeOnly)
+    project.configurations.shadow.get().dependencies.addAll(dependencies)
 }
